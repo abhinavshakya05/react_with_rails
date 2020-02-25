5fe196f8c2abda629528c69d4a4b60ee7e688ad9
Abhinav <abhinavs@flexsin.com>
2020-02-25 11:59:41 +0530

    added react router

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Home from './Home'
+import Posts from './Posts'
+import NewPost from './NewPost'
+import {Route, Switch} from 'react-router-dom'
 
 class App extends React.Component {
 	render() {
 		return (
 			<div>
-				Hello World!
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/posts" component={Posts} />
+					<Route exact path="/new_post" component={NewPost} />
+				</Switch>
 			</div>
 		)
 	}
---
+++ app/javascript/components/Posts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Posts extends React.Component {
+	render() {
+		return (
+			<div>
+				posts page
+			</div>
+		)
+	}
+}
+
+export default Posts
